2fe9d04b4eb0e156ab9a6ce2bda101ffc95bbaa4
Sai Harsha Maddela <harsha.ms7@gmail.com>
2019-02-18 19:31:18 -0800

    generic webhooks

--- src/Http/Controllers/WebhookController.php
+++ src/Http/Controllers/WebhookController.php
@@ -4,9 +4,7 @@ namespace Laravel\Cashier\Http\Controllers;
 
 use Exception;
 use Illuminate\Http\Request;
-use Laravel\Cashier\Subscription;
 use Illuminate\Routing\Controller;
-use Braintree\WebhookNotification;
 use Symfony\Component\HttpFoundation\Response;
 
 class WebhookController extends Controller
@@ -16,6 +14,48 @@ class WebhookController extends Controller
      */
     public function handleWebhook(Request $request)
     {
-        //
+        try {
+            $webhook = $this->parseWebhookNotification($request);
+        } catch (Exception $e) {
+            return;
+        }
+
+        $method = 'handle'.studly_case(str_replace('.', '_', $webhook->kind));
+        if (method_exists($this, $method)) {
+            return $this->{$method}($webhook);
+        }
+
+        return $this->missingMethod();
+    }
+
+    /**
+     *
+     */
+    public function parseWebhookNotification($request)
+    {
+        $originalHash = $request->headers->get('signature');
+        $token = $request->headers->get('token');
+        $timestamp = $request->headers->get('timestamp');
+        $clientId = config('payments.billing.client_id');
+        $algo = 'sha256';
+
+        $data = $token . $timestamp;
+        $calculatedHash = hash_hmac($algo, $data, $clientId);
+
+        if (hash_equals($originalHash, $calculatedHash)) {
+            // hashes matched, process data
+            return (object) $request->all();
+        } else {
+            throw new Exception('Hash did not match');
+        }
+    }
+
+
+    /**
+     * Handle calls to missing methods on the controller.
+     */
+    public function missingMethod($parameters = [])
+    {
+        return new Response;
     }
 }
